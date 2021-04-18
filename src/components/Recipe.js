63828da90357f7d0edcfa824857f8480c83aa97f
ThomasA64 <thomas.arthur64@gmail.com>
2021-04-18 14:21:24 -0400

    added add and delete button functions to buttons

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,27 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import IngredientList from "./IngredientList";
+import { RecipeContext } from "./App";
 
 export default function Recipe(props) {
-  const {
-    id,
-    name,
-    cookTime,
-    servings,
-    instructions,
-    ingredients,
-    handleRecipeDelete,
-  } = props;
+  const { handleRecipeDelete } = useContext(RecipeContext);
+  const { id, name, cookTime, servings, instructions, ingredients } = props;
   return (
     <div className="recipe">
       <div className="recipe__header">
         <h3 className="recipe__title">{name}</h3>
         <div>
-          <button className="btn btn--primary">Edit</button>
+          <button className="btn btn--primary mr-1">Edit</button>
           <button
             className="btn btn--danger"
             onClick={() => handleRecipeDelete(id)}
           >
-            {" "}
             Delete
           </button>
         </div>
@@ -36,13 +29,13 @@ export default function Recipe(props) {
       </div>
       <div className="recipe__row">
         <span className="recipe__label">Instructions:</span>
-        <div className="recipe__value indented instructions">
+        <div className="recipe__value recipe__instructions recipe__value--indented">
           {instructions}
         </div>
       </div>
       <div className="recipe__row">
         <span className="recipe__label">Ingredients:</span>
-        <div className="recipe__value indented instructions">
+        <div className="recipe__value recipe__value--indented">
           <IngredientList ingredients={ingredients} />
         </div>
       </div>
