e29f54fa7827dc486b5673c6d8c74e4ba32a4f29
ThomasA64 <thomas.arthur64@gmail.com>
2021-04-19 22:13:55 -0400

    changed handleRecipeAdd hook to have no default values

--- src/components/App.js
+++ src/components/App.js
@@ -37,11 +37,11 @@ function App() {
   function handleRecipeAdd() {
     const newRecipe = {
       id: uuidv4(),
-      name: "New",
+      name: "",
       servings: 1,
-      cookTime: "1:00",
-      instructions: "Instr.",
-      ingredients: [{ id: uuidv4(), name: "Name", amount: "1 Tbs" }],
+      cookTime: "",
+      instructions: "",
+      ingredients: [{ id: uuidv4(), name: "", amount: "" }],
     };
 
     setRecipes([...recipes, newRecipe]);
